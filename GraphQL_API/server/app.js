b4000a6775cf23779184c05ec53d57df54f420de
timidgeek <9liveslinds@gmail.com>
2023-11-07 19:06:27 +0000

    grabbed the json

---
+++ GraphQL_API/server/app.js
@@ -0,0 +1,11 @@
+// given app route for express
+const express = require('express');
+const {graphqlHTTP} = require('express-graphql');
+
+const app = express();
+
+app.use('/graphql',graphqlHTTP({
+}));
+app.listen(4000,()=>{
+  console.log('now listening for request on port 4000');
+});
---
+++ GraphQL_API/server/schema/schema.js
@@ -0,0 +1,8 @@
+export const typeDefs = `#graphql
+type Game{
+  id: GraphQLString,
+  title: GraphQLString,
+  weight: GraphQLInt,
+  description: GraphQLString
+}
+` 
