8cb8939192f265b9172e7355ae5ceea51fa9ad88
timidgeek <9liveslinds@gmail.com>
2023-11-12 13:13:57 -0600

    task 4 - successfully connected tasks and projects

--- GraphQL_API/server/schema/schema.js
+++ GraphQL_API/server/schema/schema.js
@@ -9,19 +9,22 @@ const {
   GraphQLString, 
   GraphQLInt, 
   GraphQLSchema,
-  GraphQLID
+  GraphQLID,
+  GraphQLList
  } = require('graphql');
 
  // tasks array with task data
  const tasks = [
   {
-    id: "1", 
+    id: "1",
+    projectId: "1",
     title: 'Create your first webpage', 
     weight: 1, 
     description: 'Create your first HTML file 0-index.html with: -Add the doctype on the first line (without any comment) -After the doctype, open and close a html tag Open your file in your browser (the page should be blank)',
   },
   {
-    id: "2", 
+    id: "2",
+    projectId: "1",
     title: 'Structure your webpage', 
     weight: 1, 
     description: 'Copy the content of 0-index.html into 1-index.html Create the head and body sections inside the html tag, create the head and body tags (empty) in this order'
@@ -47,23 +50,34 @@ const {
 // create a new GraphQLObjectType: TaskType
 const TaskType = new GraphQLObjectType({
   name: 'Task',
-  fields: {  
+  fields: () => ({  
     id: { type: GraphQLID },  
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-}
+    description: { type: GraphQLString },
+    project: { 
+      type: ProjectType,
+      resolve(parent, args){
+        return _.find(projects, { id: parent.projectId });
+      }},
+  }),
 });
 
 // create a new GraphQLObjectType: ProjectType
 const ProjectType = new GraphQLObjectType({
   name: 'Project',
-  fields: {
+  fields: () => ({
     id: { type: GraphQLID },  
     title: { type: GraphQLString },
     weight: { type: GraphQLInt },
-    description: { type: GraphQLString }
-  }
+    description: { type: GraphQLString },
+    tasks: {
+      type: new GraphQLList(TaskType),
+      resolve(parent, args){
+        return _.filter(tasks, { projectId: parent.id})
+      }
+    }
+  })
 });
 
 // create a new GraphQLObjectType: RootQuery
